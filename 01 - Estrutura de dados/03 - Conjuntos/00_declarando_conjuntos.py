44f6a51bddda52dc65069790a69a25984241b219
Uilames <uilamesa.ferreira@gmail.com>
2023-09-04 16:04:04 -0300

    estudos

--- 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
+++ 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
@@ -2,7 +2,8 @@ numeros = set([1, 2, 3, 1, 3, 4])
 print(numeros)  # {1, 2, 3, 4}
 
 letras = set("abacaxi")
-print(letras)  # {"b", "a", "c", "x", "i"}
+print(sorted(letras))  # {"b", "a", "c", "x", "i"}
 
 carros = set(("palio", "gol", "celta", "palio"))
+
 print(carros)  # {"gol", "celta", "palio"}
